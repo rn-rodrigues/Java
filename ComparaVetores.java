0f8e866e8ec7344b0e8e680f9492ad2fee1f11c0
Renan Rodrigues <renanrod.dias@gmail.com>
2019-05-11 12:40:21 -0300

    Update ComparaVetores.java

--- ComparaVetores.java
+++ ComparaVetores.java
@@ -26,18 +26,32 @@ public class Vetores {
     }
     
     public static void main(String[] args) {
-		int x[];
-		int y[];
-		Scanner input = new Scanner(System.in);      
-		System.out.println("Digite os elementos do Vetor x: " + "\n");
-        x = new int[input];
-    
-        System.out.println("Idade: "+"\n");             //Leitura da Idade da Pessoa
-        int idade = input.nextInt();
-		Scanner x = new Scanner (System.in);
+		String fim = new String("fim");
+		int i=0;
+		while (fim != "fim") {
+		    Scanner tamanho = new Scanner(System.in);
+		    
+		    System.out.println("Qual tamanho do vetor x?:");
+		    int tamX = tamanho.nextInt();
+		    int x[] = new int[tamX];
+		    
+		    System.out.println("Qual tamanho do vetor y?:");
+		    int tamY = tamanho.nextInt();
+		    int y[] = new int[tamY];
+
+            Scanner input = new Scanner(System.in);		          
+		    System.out.println("Digite" + i+1 + "º" + "dos elementos do Vetor x: " + "\n");
+            x[i] = input.nextInt();
+            
+            System.out.println("Digite" + i+1 + "º" + "dos elementos do Vetor y: " + "\n");
+            y[i] = input.nextInt();
+            i++;
+            
+            System.out.println("Fim dos vetores ?");
+            String fim = input.nextLine();
+		}
 		Vetores vetores = new Vetores();
 		Comparacao cVetor = vetores.new Comparacao();
 		boolean testeDeComparacao = cVetor.compararVetor(int x[], int y[]);
-		
 	}
 }
